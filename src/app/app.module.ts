3e1facd025046b94c088d9cc72511b7a8d49ff15
Carolina Labastida Meza <carolina.labastida@nectri.com>
2023-03-22 01:03:59 -0600

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { PageWrapperComponent } from './page-wrapper/page-wrapper.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SidebarComponent,
+    ToolbarComponent,
+    PageWrapperComponent
   ],
   imports: [
     BrowserModule
